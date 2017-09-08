c0c62e61ab306402897ccc5f28bdb6f70fd1fa7a
Ivor Blockley <ivor.blockley@bom.gov.au>
2017-09-08 18:32:42 +1000

    TIDY: rename test_producer.py test_TaskEvent.py. The hope is this example will soon consume the event it just produced... and thereby be somewhat more useful as an example

--- run_python_example.sh
+++ run_python_example.sh
@@ -6,4 +6,4 @@ PP=build/generated/source/proto/main/python
 
 export PYTHONPATH=$ROOT/$PP:$ROOT/$PP/common:$ROOT/$PP/common/enums
 
-python $ROOT/src/python/test_producer.py
+python $ROOT/src/python/test_TaskEvent.py
